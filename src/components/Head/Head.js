3d687a5662ca1b19b12ca1cf80abd727a8e8452d
Rif-7 <muhammadrifan974530@gmail.com>
2023-10-15 20:19:37 +0530

    add gsap animation to head

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,11 +1,38 @@
 import { Box, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import Spline from "@splinetool/react-spline";
 import SocialLinks from "./SocialLinks";
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
 
 export default function Head() {
+  const container = useRef(null);
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.from(".header-logo", {
+        duration: 1,
+        x: "-100%",
+        delay: 0.5,
+      });
+
+      gsap.from(".social-icon", {
+        duration: 1,
+        y: "100%",
+        opacity: 0,
+        delay: 0.5,
+      });
+    }, container);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <Box position={"relative"}>
-      <HStack position={"absolute"} paddingX={"30px"} paddingY={"15px"}>
+    <Box position={"relative"} ref={container}>
+      <HStack
+        position={"absolute"}
+        paddingX={"30px"}
+        paddingY={"15px"}
+        className="header-logo"
+      >
         <Image src="navlogo.png" qalt="logo" w={{ base: "60px", md: "70px" }} />
         <VStack
           gap={"0px"}
--- src/components/Head/SocialLinks.js
+++ src/components/Head/SocialLinks.js
@@ -10,10 +10,34 @@ export default function SocialLinks() {
       justify={"center"}
     >
       <HStack gap={"10px"} padding={"5px"} bg={"whiteAlpha.600"} rounded={"md"}>
-        <Image src={"linkedin.png"} w={"50px"} h={"50px"} alt="Linkedin Logo" />
-        <Image src={"github.png"} w={"50px"} h={"50px"} alt="Github Logo" />
-        <Image src={"mail.png"} w={"50px"} h={"50px"} alt="Email Logo" />
-        <Image src={"resume.png"} w={"50px"} h={"50px"} alt="Email Logo" />
+        <Image
+          className="social-icon"
+          src={"linkedin.png"}
+          w={"50px"}
+          h={"50px"}
+          alt="Linkedin Logo"
+        />
+        <Image
+          className="social-icon"
+          src={"github.png"}
+          w={"50px"}
+          h={"50px"}
+          alt="Github Logo"
+        />
+        <Image
+          className="social-icon"
+          src={"mail.png"}
+          w={"50px"}
+          h={"50px"}
+          alt="Email Logo"
+        />
+        <Image
+          className="social-icon"
+          src={"resume.png"}
+          w={"50px"}
+          h={"50px"}
+          alt="Email Logo"
+        />
       </HStack>
     </HStack>
   );
