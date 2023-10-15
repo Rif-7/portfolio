fa70392faa16f649d6b0606d4318f12471097858
Rif-7 <muhammadrifan974530@gmail.com>
2023-10-15 17:33:18 +0530

    refractor SkillSet into a separate component

--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -1,7 +1,11 @@
-import { Flex, Image, Text, VStack } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
 import { languages, otherSkills } from "./skillData";
+import { useRef } from "react";
+import SkillSet from "./SkillSet";
 
 export default function Skills() {
+  const container = useRef();
+
   return (
     <VStack
       maxW={"800px"}
@@ -9,6 +13,7 @@ export default function Skills() {
       mt={"25px"}
       align={"center"}
       justify={"center"}
+      ref={container}
     >
       <Text
         className="space"
@@ -21,53 +26,6 @@ export default function Skills() {
         <SkillSet skills={languages} title={"Languages"} />
         <SkillSet skills={otherSkills} title={"Frameworks & Tools"} />
       </VStack>
-    </VStack>
-  );
-}
-
-function SkillSet({ skills, title }) {
-  return (
-    <VStack rounded={"md"} color={"white"} align={"center"} justify={"center"}>
-      <Text
-        className="space"
-        fontSize={{ base: "24px", md: "28px" }}
-        fontWeight={"semibold"}
-      >
-        {title}
-      </Text>
-      <Flex
-        wrap={"wrap"}
-        gap={{ base: "10px", md: "18px" }}
-        padding={{ base: "0px", md: "18px" }}
-        justify={"center"}
-        align={"center"}
-      >
-        {skills.map((skill, index) => (
-          <VStack
-            key={index}
-            rounded={"md"}
-            w={{ base: "80px", md: "100px" }}
-            h={{ base: "80px", md: "100px" }}
-            bg={"white"}
-            color={"black"}
-            align={"center"}
-            justify={"center"}
-            padding={{ base: "4px", md: "8px" }}
-          >
-            <Image
-              alt={skill.name}
-              src={skill.url}
-              w={{ base: "40px", md: "50px" }}
-            />
-            <Text
-              fontSize={{ base: "14px", md: "16px" }}
-              fontWeight={"semibold"}
-            >
-              {skill.name}
-            </Text>
-          </VStack>
-        ))}
-      </Flex>
     </VStack>
   );
 }
