ed6a0449445b80f203336348c8666f1dc05ae9f0
Rif-7 <muhammadrifan974530@gmail.com>
2023-10-24 22:08:12 +0530

    add hover effect to social links

--- src/components/Head/SocialLinks.js
+++ src/components/Head/SocialLinks.js
@@ -1,26 +1,48 @@
-import { Center, Flex, HStack, Icon, Link } from "@chakra-ui/react";
+import { Center, Flex, HStack, Icon, Link, Text } from "@chakra-ui/react";
 import socials from "../../links";
 
 import { TbBrandGithubFilled } from "react-icons/tb";
 import { BiLogoLinkedin, BiSolidUserDetail } from "react-icons/bi";
 import { MdEmail } from "react-icons/md";
+import { useState } from "react";
 
 export default function SocialLinks() {
   return (
     <HStack position={"absolute"} top={"30px"} left={"10px"} width={"100%"}>
       <Flex direction={"column"} gap={"10px"} padding={"5px"} rounded={"md"}>
-        <SocialIcon link={socials.github} icn={TbBrandGithubFilled} />
-        <SocialIcon link={socials.linkedin} icn={BiLogoLinkedin} />
-        <SocialIcon link={socials.email} icn={MdEmail} />
-        <SocialIcon link={socials.resume} icn={BiSolidUserDetail} />
+        <SocialIcon
+          link={socials.github}
+          name={"Github"}
+          icn={TbBrandGithubFilled}
+        />
+        <SocialIcon
+          link={socials.linkedin}
+          name={"Linkedin"}
+          icn={BiLogoLinkedin}
+        />
+        <SocialIcon link={socials.email} name={"Email"} icn={MdEmail} />
+        <SocialIcon
+          link={socials.resume}
+          name={"Resume"}
+          icn={BiSolidUserDetail}
+        />
       </Flex>
     </HStack>
   );
 }
 
-function SocialIcon({ link, icn }) {
+function SocialIcon({ link, icn, name }) {
+  const [showName, setShowName] = useState(false);
+
   return (
-    <Link href={link} color={"gray.700"} isExternal>
+    <Link
+      href={link}
+      color={"gray.700"}
+      isExternal
+      pos={"relative"}
+      onMouseOver={() => setShowName(true)}
+      onMouseLeave={() => setShowName(false)}
+    >
       <Center
         padding={"4px"}
         border={"6px solid #2D3748"}
@@ -29,6 +51,15 @@ function SocialIcon({ link, icn }) {
       >
         <Icon as={icn} boxSize={{ base: "6", md: "8" }} />
       </Center>
+      <Text
+        fontSize={"15px"}
+        top={"20%"}
+        pos={"absolute"}
+        left={"60px"}
+        visibility={showName ? "visible" : "hidden"}
+      >
+        {name}
+      </Text>
     </Link>
   );
 }
