a5b2271ed8f5d3d93d6d6ae15ab6ba08f7cdeaf3
Rif-7 <muhammadrifan974530@gmail.com>
2023-10-24 13:08:36 +0530

    add resume

--- src/links.js
+++ src/links.js
@@ -1,7 +1,7 @@
 const socials = {
   github: "https://github.com/Rif-7",
   linkedin: "https://www.linkedin.com/in/rifan-na",
-  resume: "",
+  resume: "resume/myresume.pdf",
   email: "mailto:rifanna70@gmail.com",
 };
 
